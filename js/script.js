238f8ecf7ca0911a045a32d4fc6afd49380b6172
Toni Powell <tonipow3ll@gmail.com>
2021-01-17 15:17:28 -0500

    styling, date shows

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
-// function for adding events
-// display for showing hours in a day (9-5PM)
-// function for adding events to certain hours
+// function to show time hours on the left side of page
+//      gray out hours if 'past' hour.. ex 10AM - 9AM will be grayed out
+// 
+// function for SAVING events to certain hours
 // function for saving events to local storage
 // func for creating elements?!
 // 
 
 
+//later - fix your buttons 
 
 $(document).ready(function (){
+
+// todays Date / current time 
+const currentDate = dayjs().format('MMM dddd, D');
+$("#currentDay").text(currentDate);
+
+const currentTime = dayjs().format('h:mm');
+$("#currentTime").text(currentTime);
+
+
+// below creates 'node'list, with id of currentDay/Time/
+
+
 
 });
