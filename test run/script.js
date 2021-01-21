01588ebeecd81d46bc47731b0979774de55f8e0d
Toni Powell <tonipow3ll@gmail.com>
2021-01-21 13:14:24 -0500

    test running

--- test run/script.js
+++ test run/script.js
@@ -9,7 +9,6 @@ const currentTime = dayjs().format('h:mm');
 
 let storageInput = document.querySelector('.storage')
 let text = document.querySelector('.text')
-let button = document.querySelector('.eventBtn')
 let storedEvents = localStorage.getItem('userInput')
 let userEvents = []
 
@@ -20,16 +19,47 @@ storageInput.addEventListener('input', letters => {
     text.textContent = letters.target.value
   //  console.log(letters.target.value)
 })
+
+// writes items from local storage to page 
 const saveEvents = () => {
     localStorage.setItem('userInput', text.textContent)
 }
 
-$(".eventBtn").on("click", function(event){
+// listens for click on 'event buttons' 
+
+// $(".eventBtn").on("click", function(event){
+//     event.preventDefault();
+//     let newEvents = $(".text").val().trim();
+//     userEvents.push(newEvents);
+//     saveEvents();
+// })
+
+// below will write to storage, AND save the 'event' to local storage
+$("#9AM").on("click", function(event){
     event.preventDefault();
     let newEvents = $(".text").val().trim();
+    let pastEvents = JSON.stringify(storedEvents);
+    // let para = $("<p>").append(newEvents); 
+    //will need the below for 'past' events
+    let para = $("<p>").text("9AM event: " + $("input").val());
     userEvents.push(newEvents);
+    $("#write").append(para)
     saveEvents();
 })
 
+
  // end script
 });
+
+// --------------NOTES 
+
+//  // Creating a div to hold the movie
+//  var movieDiv = $("<div class='movie'>");
+
+// // Creating an element to have the rating displayed
+// var pOne = $("<p>").text("Rating: " + rating);
+
+// // Displaying the rating
+// movieDiv.append(pOne);
+
+// when 9AM button is clicked, save event and show '9AM -i have stuff to do here- 
